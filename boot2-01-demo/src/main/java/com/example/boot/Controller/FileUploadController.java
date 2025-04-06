f44f30a2abafde61106ac526180ff64b1f15be89
yezi <yezihere@qq.com>
2025-04-06 21:49:27 +0800

    文件上传并访问的尝试

--- boot2-01-demo/src/main/java/com/example/boot/Controller/FileUploadController.java
+++ boot2-01-demo/src/main/java/com/example/boot/Controller/FileUploadController.java
@@ -13,18 +13,33 @@ package com.example.boot.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import javax.servlet.http.HttpServletRequest;
 
+
+import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.Phaser;
 
 @RestController
 public class FileUploadController {
     @PostMapping("/upload")
-    public String up(String nickname, MultipartFile photo, HttpSerlvetRequest request)throws IOException {
+    public String up(String nickname, MultipartFile photo, HttpServletRequest request)throws IOException {
         nickname = nickname.trim();
         System.out.println(nickname);
         System.out.println(photo.getOriginalFilename());
         System.out.println(photo.getContentType());
         String path = request.getServletContext().getRealPath("/upload/");
+        System.out.println(path);
+        saveFile(photo,path);
         return "上传成功";
+    }
+
+    public void saveFile(MultipartFile photo,String path)throws IOException {
+        File dir = new File(path);
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+        File file = new File(path + photo.getOriginalFilename());
+        photo.transferTo(file);
     }
 }
